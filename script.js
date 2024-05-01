86b1213daaefe6e87dc55c8dc6dc0f4ebe9d5b77
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-01 12:34:09 +0200

    coding challenge 05 (strings)

--- script.js
+++ script.js
@@ -214,3 +214,36 @@ const convertToCamelCase = function (string) {
 };
 
 convertToCamelCase(testData);
+
+//////////////////////////////////////////
+// Data Structures CODING CHALLENGE #05 //
+//////////////////////////////////////////
+
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// OUTPUT:
+//
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//               Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//             Departure from FAO to LIS (12h30)
+
+const flightsConversion = function (str) {
+  const rows = str.split("+");
+
+  const dest = (d) => d.slice(0, 3).toUpperCase();
+
+  for (const row of rows) {
+    const [info, from, to, time] = row.split(";");
+    const output = `${info.includes("Delayed") ? "🔴" : ""} ${info
+      .slice(1)
+      .replace("_", " ")} from ${dest(from)} to ${dest(to)} (${time.replace(
+      ":",
+      "h"
+    )})`;
+    console.log(output.padStart(50));
+  }
+};
+
+flightsConversion(flights);
